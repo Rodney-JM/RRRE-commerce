605d2910e63a511c88beb30ef38d73ef02479ac7
Rodney <rondineypatricio@gmail.com>
2024-07-27 16:07:19 -0300

    color pallet changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,16 @@ module.exports = {
       colors:{
         "blackS": "#3B3936",
         "white1": "#F2F2F2",
-        "white2": "#889C9B",
+        "white2": "#d0d0d0",
         "white3": "#486966",
+        "white4": "#7c7989",
         "Esc": "#4951F2",
         "Esc2": "#5C73F2",
         "Norm": "#BF045B",
         "Clar": "#7E8FF2",
         "Clar2": "#BBC3F2",
+        "registerBg": "#2f2f35",
+        "registerInputElements":"#4D5159"
       }
     },
   },
